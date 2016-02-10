cddf7e4e5bd27cf2b37baf2ce3f7723a064d46ce
Jun Imura <imura@xcoo.jp>
2016-02-10 16:43:33 +0900

    Upgrade clojure and libraries.

--- project.clj
+++ project.clj
@@ -3,11 +3,11 @@
   :url "https://github.com/chrovis/clj-bwa"
   :license {:name "Apache License, Version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [net.java.dev.jna/jna "4.1.0"]
-                 [camel-snake-kebab "0.2.4"]]
-  :plugins [[lein-midje "3.1.3"]]
-  :profiles {:dev {:dependencies [[midje "1.6.3"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [net.java.dev.jna/jna "4.2.1"]
+                 [camel-snake-kebab "0.3.2"]]
+  :plugins [[lein-midje "3.2"]]
+  :profiles {:dev {:dependencies [[midje "1.8.3"]
                                   [me.raynes/fs "1.4.6"]]
                    :global-vars {*warn-on-reflection* true}}}
   :source-paths ["src/clojure"]
